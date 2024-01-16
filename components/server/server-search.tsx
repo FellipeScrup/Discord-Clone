e4c2e26e0e76071a05bddc6b7e931ccbbc212965
FellipeScrup <fellipescruph@gmail.com>
2024-01-16 12:21:49 -0300

    Agora é possivel editar canais e deletalos, foi adicionado  a função que agora sempre que a pessoa clicar ou entra no server, ela será imediatamente redirecionada para o chat de texto com nome de 'general'

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -67,7 +67,7 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
             Search
           </p>
           <kbd className="pointer-events-none inline-flex h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium text-muted-foreground ml-auto">
-            <span className="text-xs">CLICK</span>
+            <span className="text-xs">CTRL</span>K
           </kbd>
         </button>
         <CommandDialog open={open} onOpenChange={setOpen}>
